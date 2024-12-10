d0a54b6bf9c50a81d64e52d2ace60be4cad177f2
CHZelaya <chzelaya1201@gmail.com>
2024-12-10 13:11:09 -0700

    Resolves #27 - Resolves #28 -- Successfully populating agents on website, statically adding images, something to consider explaining when presenation time comes

---
+++ TravelExpertsMVC/Controllers/AgentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelExpertsData.Manager;
+using TravelExpertsData.Models;
+
+namespace TravelExpertsMVC.Controllers
+{
+    public class AgentController : Controller
+    {
+        private readonly TravelExpertsContext _context;
+
+        public AgentController(TravelExpertsContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IActionResult Index()
+        {
+            List<Agent> agents = AgentManager.GetAllAgents(_context);
+            return View(agents);
+        }
+    }
+}
